b510cf4c9435fe6626a11c3612179b182d8ccf03
Edwin <evdbelt@hotmail.com>
2025-06-10 14:34:52 +0200

    First attempt for OSDM wrapper

---
+++ osdm-wrapper/README.md
@@ -0,0 +1,70 @@
+# OSDM wrapper
+
+In this directory, you'll find 2 tools: one to simulate an OSDM server, and another that wraps it and publishes an OMSA interface.  
+This is an example demo setup, and not usable for production environments.
+
+- [OSDM wrapper](#osdm-wrapper)
+  - [Installation guide](#installation-guide)
+  - [OSDM Server](#osdm-server)
+    - [Start](#start)
+    - [Test](#test)
+    - [Implementation details](#implementation-details)
+  - [OMSA wrapper server](#omsa-wrapper-server)
+    - [Start](#start-1)
+    - [Test](#test-1)
+    - [Implementation details](#implementation-details-1)
+
+## Installation guide
+
+Both tools are build with Node.js, this is what you have to install at first.  
+When installed, you have to open a terminal (or powershell), and navigate to this folder.  
+Execute `npm install jsonata express @apidevtools/swagger-parser openapi-sampler axios`.  
+You're ready!
+
+## OSDM Server
+
+### Start
+
+To start the OSDM server, open a new terminal/shell, navigate to this folder.
+Execute `node ./osdm.js`.
+
+### Test
+
+You can test it, see if it works by executing in another terminal this command:  
+`curl -X POST http://localhost:3001/v1/product`
+
+It will return a JSON result.
+
+### Implementation details
+
+This server is created using the OpenAPI Sampler library, generating a JSON output that complies to the BoB definition in the OpenAPI specification (expressed in the file OSDM-350.yaml).  
+  
+If you want to replace this generated output, you'll have to modify the osdm.js, for instance to publish JSON output provided in files.
+
+## OMSA wrapper server
+
+### Start
+
+To start the OMSA server, open a new terminal/shell, navigate to this folder.
+Execute `node ./omsa.js`.
+
+### Test
+
+You can test it, see if it works by executing in another terminal this command:  
+`curl -X GET http://localhost:3000/v1/`
+
+It will return a JSON result, representing the landing page.
+
+### Implementation details
+
+It created using the JSONATA library. Please have a look to understand what it does.  
+Source: https://www.npmjs.com/package/jsonata
+Reference implementation of the [JSONata query and transformation language](http://jsonata.org/).
+
+* [JSONata in 5 minutes](https://www.youtube.com/embed/ZBaK40rtIBM)
+* [JSONata language documentation](http://docs.jsonata.org/)
+* [Try it out!](http://try.jsonata.org/)
+
+When the server is started, the OMSA OpenAPI specification is read and the endpoints are initiated whenever a corresponding file is found in the `/requests` folder.
+
+If you want to extend this example, or just want to modify the mapping (requests from OMSA->OSDM or responses from BoB->OSDM), you'll have to modify the files in the `/requests` or `/responses` folders. The name of the file corresponds with the operationId from the OMSA endpoint.
---
+++ osdm-wrapper/omsa.js
@@ -0,0 +1,222 @@
+const express = require('express');
+const SwaggerParser = require('@apidevtools/swagger-parser');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const jsonata = require('jsonata');
+const fs = require('fs');
+const endpointMap = require(path.join(__dirname, 'endpoint-mapping.json'));
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+const requestsDir = path.join(__dirname, 'requests');
+const responsesDir = path.join(__dirname, 'responses');
+
+// Laad en dereference de OpenAPI spec
+const openApiV2 = path.join(__dirname, 'OMSA-001.yaml');
+
+async function postToOSDM(url, body) {
+  console.log(`calling http://localhost:3001${url}`);
+  try {
+    const response = await axios.post('http://localhost:3001' + url, body);
+    return response
+  } catch (error) {
+    console.error('Fout:', error.message);
+  }
+}
+
+function addPost(baseRoutePath, methods) {
+    if (methods.post && methods.post.operationId) {
+        toAdd = {}
+        operation = methods.post.operationId;
+        if (baseRoutePath.includes('{')) {
+            methods.post.parameters.forEach((p, i) => {
+                if (p.in === 'path' && p.schema?.enum){
+                    p.schema.enum.forEach( (enumValue, i2) => {
+                        o2 = operation + '-' + enumValue;
+                        r2 = baseRoutePath.replace('{' + p.name + '}', enumValue );
+                        toAdd[o2] = r2;
+                    });
+                }
+            });
+        }
+        else {
+            toAdd[operation] = baseRoutePath;
+        }
+
+        Object.entries(toAdd).forEach( ([operationId, routePath]) => {
+            const requestPath = path.join(requestsDir, `${operationId}.jsonata`);
+            const responsePath = path.join(responsesDir, `${operationId}.jsonata`);
+
+            if( !fs.existsSync(requestPath) )
+                return;
+
+            app.post(routePath, (req, res) => {
+                const data = req.body;
+                data.bbox = req.params.bbox ? req.params.bbox : [0,0,1,1];
+
+                fs.readFile(requestPath, 'utf8', (err, expressionText) => {
+                    if (err) {
+                        return res.status(500).json({ error: `Kan expressie niet lezen voor '${operationId}': ${err.message}` });
+                    }
+
+                    try {
+                        (async() => { 
+                            const expression = jsonata(expressionText);
+                            body = await expression.evaluate(data);
+                            
+                            url = endpointMap[operationId];
+
+                            if (url === 'none') {
+                              res.json(body);
+                              return;
+                            }
+
+                            if (url instanceof Array){
+                                result1 = await callUrl(url[0]);
+                                res.json(await callUrl(url[1]));
+                                return;
+                            }
+
+                            result = callUrl(url);
+
+                            // if (url.includes("{") ) {
+                            //     const match = url.match(/{([^}]+)}/);
+                            //     if (match) {
+                            //         toFind = match[1];
+                            //     }
+                            //     id = data.inputs[toFind];
+                            //     url = url.replace(/{(\w+)}/g, id);
+                            // }
+
+                            // url = url.replace('POST ', '').replace('GET ', '');
+
+                            // result = await postToOSDM(url, body)
+                            //     .then( a => 
+                            //         {
+                            //             v16result = a.data;
+                            //             fs.readFile(responsePath, 'utf8', (err, responseJsonata) => {
+                            //                 (async() => {
+                            //                     const responseExpression = jsonata(responseJsonata);
+                            //                     result = await responseExpression.evaluate(v16result);
+                            //                     return result;
+                            //                 })()}
+                            //             ) 
+                            //         } );
+                            res.json(result);
+                        })()
+                    } catch (e) {
+                        res.status(400).json({ error: `Fout in JSONata expressie: ${e.message}` });
+                    }
+                });
+            });
+            msg = `Endpoint geladen: POST ${routePath} => request/${operationId}.jsonata`;
+
+            if (operationId in endpointMap) {
+                msg += ' | ' + endpointMap[operationId] + ` | response/${operationId}.jsonata`
+            }
+            
+            console.log(msg);
+        } );
+    }
+}
+
+async function callUrl(url) {
+    if (url.includes("{") ) {
+        const match = url.match(/{([^}]+)}/);
+        if (match) {
+            toFind = match[1];
+        }
+        id = data.inputs[toFind];
+        url = url.replace(/{(\w+)}/g, id);
+    }
+
+    url = url.replace('POST ', '').replace('GET ', '');
+
+    result = await postToOSDM(url, body)
+        .then( a => 
+            {
+                v16result = a.data;
+                fs.readFile(responsePath, 'utf8', (err, responseJsonata) => {
+                    (async() => {
+                        const responseExpression = jsonata(responseJsonata);
+                        result = await responseExpression.evaluate(v16result);
+                        return result;
+                    })()}
+                ) 
+            } );
+    res.json(result);
+}
+
+function addGet(routePath, methods) {
+    if (methods.get && methods.get.operationId) {
+        const operationId = methods.get.operationId;
+        const requestPath = path.join(requestsDir, `${operationId}.jsonata`);
+
+        msg = `Endpoint geladen: GET ${routePath} => ${operationId}.jsonata`
+        if (operationId == 'openApi') {
+            msg = `Endpoint geladen: GET ${routePath} => TOMP-API.yaml`
+        }
+
+        if(operationId != 'openApi' && !fs.existsSync(requestPath))
+            return;
+
+        app.get(routePath, (req, res) => {
+            const data = req.query;
+
+            if (operationId == 'openApi' ){
+                fs.readFile(openApi, (err, content) => { 
+                    res.send(content); 
+                } );
+                return;
+            }
+
+            fs.readFile(requestPath, 'utf8', (err, expressionText) => {
+                if (err) {
+                    return res.status(500).json({ error: `Kan expressie niet lezen voor '${operationId}': ${err.message}` });
+                }
+
+                    (async() => { 
+                      try {
+                        const expression = jsonata(expressionText);
+                        const result = await expression.evaluate(data);
+                        res.json(result);
+                      } catch (e) {
+                        res.status(400).json({ error: `Fout in JSONata expressie: ${e.message}` });
+                      }
+                    })()                    
+            });
+        });
+
+        console.log(msg);
+    }
+}
+
+app.use(bodyParser.json());
+
+// Laad en parse de OpenAPI specificatie
+if( !fs.existsSync(openApiV2) )
+    return;
+
+SwaggerParser.dereference(openApiV2)
+  .then(api => {
+    console.log('OpenAPI succesvol geladen en dereferenced.');
+    setupServer(api); 
+  })
+  .catch(err => {
+    console.error('Fout bij parsen/dereferencing van OpenAPI:', err.message);
+    process.exit(1);
+  });
+
+function setupServer(openapi) {
+    for (const [routePath, methods] of Object.entries(openapi.paths)) {
+        route = '/v1' + routePath;
+        addPost(route, methods);
+        addGet(route, methods);
+    }
+}
+
+app.listen(port, () => {
+  console.log(`Server draait op http://localhost:${port}`);
+});
